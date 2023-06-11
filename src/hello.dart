2c73de3244133b310bc6b6e8d3a2f1f8fddd6625
Sazid <70763173+shahriar00@users.noreply.github.com>
2023-06-12 01:50:28 +0600

    hello.dart

--- src/hello.dart
+++ src/hello.dart
@@ -5,8 +5,6 @@ void main(){
   var name = "Shakil";
   var roll = 32;
   double cgpa = 2.22;
-
   //This is a variable name printing and roll.............
-
   print("My name is ${name} and my roll is ${roll} cgpa ${cgpa}");
 }
