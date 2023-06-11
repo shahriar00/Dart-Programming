2ae056c46affec5b4cbb97b342afee904bd26772
shahriarsazid <sazid1190849@gamil.com>
2023-06-12 01:44:51 +0600

    hello

---
+++ src/hello.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Hello World");
+  print("This the first program of dart programing language.");
+
+  var name = "Shakil";
+  var roll = 32;
+
+  //This is a variable name printing and roll.............
+
+  print("My name is ${name} and my roll is ${roll}");
+}
