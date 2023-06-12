cea09000e84c48cda893e4fed0cfa1b26631be36
shahriarsazid <sazid1190849@gamil.com>
2023-06-12 12:24:11 +0600

    loop

---
+++ src/loop.dart
@@ -0,0 +1,40 @@
+void main()
+{
+  // for(int i = 1;i<10;i++)
+  //   {
+  //     print("${i} \n");
+  //   }
+  // int i ,j ;
+  //
+  // var table_number=2;
+  // var maximum_number=10;
+  //
+  // for(i = 1;i<=table_number;i++)
+  //   {
+  //     for(j=0;j<=maximum_number;j++){
+  //       print("${i} * ${j} = ${i*j}\n");
+  //     }
+  //   }
+  // var sum = 0;
+  // int i ;
+  // var listdata = [10,2,3,4,5];
+  //
+  // for(i in listdata){
+  //   print("Summation of ${sum+=i}");
+  // }
+
+  //While Loop
+  int i = 1;
+  while(i<10)
+    {
+      print(i);
+      i++;
+    }
+//Do While Loop
+    do{
+      print(i);
+      i++;
+
+    }while(i<20);
+
+}
