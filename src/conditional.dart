1f523be05d8f1edb42c9dd120721de70d236299b
shahriarsazid <sazid1190849@gamil.com>
2023-06-12 12:05:52 +0600

    condition

---
+++ src/conditional.dart
@@ -0,0 +1,51 @@
+void main()
+{
+  var num = 80;
+  //
+  // if(num%2 == 0)
+  //   {
+  //     print("This is a even number");
+  //   }
+  // else
+  //   {
+  //     print("Odd number.");
+  //   }
+
+  if(num>80)
+    {
+      print("The result is excellent");
+    }
+  else if(num <=80){
+    print("The result is good");
+  }
+  else{
+    print("This is average reslut");
+  }
+
+
+  var value=0;
+
+  switch(value){
+    case 1:
+      print("This value${value}");
+      break;
+    case 2:
+      print("This value${value}");
+      break;
+    case 3:
+      print("This value${value}");
+      break;
+    case 4:
+      print("This value${value}");
+      break;
+    case 5:
+      print("This value${value}");
+      break;
+    case 6:
+      print("This value${value}");
+      break;
+
+  }
+
+}
+
