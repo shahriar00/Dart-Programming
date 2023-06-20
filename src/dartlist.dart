9ea45a72a6fb335ced4b2a5ca1ba1fe768e9a709
shahriarsazid <sazid1190849@gamil.com>
2023-06-20 13:22:45 +0600

    list

---
+++ src/dartlist.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  var list1 = [];
+  list1.add(2);
+  list1.add(3);
+  // list1.addAll([3,5,6]);
+  // list1.removeAt(3);
+  // list1.remove(3);
+  // list1.removeLast();
+
+
+  var list2 = ["Atik","Faysal","Sabbir","Rafiq"];
+
+  list2.forEach((element) {
+    print("This is name list:${element}");
+  });
+
+  print(list1);
+
+}
