1433ada00da00d4dd529ae322b639a47f8576dae
shahriarsazid <sazid1190849@gamil.com>
2023-06-21 21:21:20 +0600

    sets

---
+++ src/dartset.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  var names = {"peter","jhon","Alex","simi"};
+
+  print(names);
+
+  var x = <int>{10,11,12,13,14,15};
+  var y = <int>{12,18,29,43};
+  var z = <int>{2,5,10,11,32};
+  print("Example - Set Operations");
+
+  print("x union y is -");
+  print(x.union(y));
+
+  print("x intersection y is - ");
+  print(x.intersection(y));
+
+  print("y difference z is - ");
+  print(y.difference(z));
+
+
+
+}
