5cbea4a9679e6711eab04d5e89dd0f22b015d963
shahriarsazid <sazid1190849@gamil.com>
2023-06-21 21:33:38 +0600

    map

---
+++ src/dartmap.dart
@@ -0,0 +1,33 @@
+void main()
+{
+  var student = new Map();
+  student['name'] = 'Tom';
+  student['age'] = 23;
+  student['course'] = 'B.tech';
+  student['Branch'] = 'Computer Science';
+  print(student);
+
+  // Get all Keys
+  print("The keys are : ${student.keys}");
+
+  // Get all values
+  print("The values are : ${student.values}");
+
+  // Length of Map
+  print("The length is : ${student.length}");
+
+//isEmpty function
+  print(student.isEmpty);
+
+//isNotEmpty function
+  print(student.isNotEmpty);
+
+
+  var details = new Map();
+
+  details['name'] = "Asik";
+  details['age'] = 322;
+  details['gpa'] = 4.4;
+
+  print(details);
+}
