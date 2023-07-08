10ec0c089a5971df3c7000be72be111500ef3e8e
shahriarsazid <sazid1190849@gamil.com>
2023-07-08 15:09:56 +0600

    constructor

---
+++ src/constructor_OOP.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  var mobile = new Mobilename("IPhone 12 Pro", "2020");
+
+}
+
+class Mobilename{
+  var modelname;
+  var man_year;
+
+  Mobilename(modelname,man_year)
+  {
+    this.modelname = modelname;
+    this.man_year = man_year;
+    print("${modelname},${man_year}");
+
+  }
+
+
+}
