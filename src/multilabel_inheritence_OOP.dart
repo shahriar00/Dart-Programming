5b4575a41626420757986349c5fe56711cb2186e
shahriarsazid <sazid1190849@gamil.com>
2023-07-26 11:07:31 +0600

    multilabel

---
+++ src/multilabel_inheritence_OOP.dart
@@ -0,0 +1,20 @@
+class car{
+  var name ;
+  var date;
+
+  void carinformation(){
+    print("This is a car of BMW");
+  }
+}
+
+class audi extends car{
+  void audiinformation(){
+    print("This is a audi information");
+  }
+}
+
+class nessan extends car{
+  void nessaninformation(){
+    print("This is a nessan information");
+  }
+}
