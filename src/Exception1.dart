71690f7ba7a4c79c644a3ada5bd0771dd2f9a747
shahriarsazid <sazid1190849@gamil.com>
2023-08-31 11:24:05 +0600

    exception1

---
+++ src/Exception1.dart
@@ -0,0 +1,28 @@
+void main() {
+  int x = 12;
+  int y = 0;
+  int res;
+
+  try {
+    res = x ~/ y;
+  }
+// It returns the built-in exception related to the occurring exception
+  catch(E) {
+    print(E);
+  }
+}
+
+
+// void main() {
+//   try {
+//     check_marks(-10);
+//   }
+//   catch(e) {
+//     print('The marks cannot be negative');
+//   }
+// }
+// void check_marks(int marks) {
+//   if(marks<0) {
+//     throw new FormatException();  // Raising explanation externally
+//   }
+// }
