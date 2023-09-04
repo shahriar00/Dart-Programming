9721aefd7f557409eb211566af6598e17f481462
shahriarsazid <sazid1190849@gamil.com>
2023-09-04 17:03:07 +0600

    exception2

---
+++ src/Exception2.dart
@@ -0,0 +1,34 @@
+main() {
+  try {
+    check_marks(-10);
+  }
+  catch(e) {
+    print('The marks cannot be negative');
+  }
+}
+void check_marks(int marks) {
+  if(marks<0) {
+    throw new FormatException();  // Raising explanation externally
+  }
+}
+
+
+// class AmtException implements Exception {
+//   String expMsg() => 'Entered Amount should be greater than zero';
+// }
+// void main() {
+//   try {
+//     withdraw_amt(-1);
+//   }
+//   catch(E) {
+//     print(E.expMsg());
+//   }
+//   finally {
+//     print('Ending requested operation.....');
+//   }
+// }
+// void withdraw_amt(int amt) {
+//   if (amt <= 0) {
+//     throw new AmtException();
+//   }
+// }
