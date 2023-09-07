191d372fcefa01af063a122ba7b6919cac521fed
shahriarsazid <sazid1190849@gamil.com>
2023-09-07 12:51:26 +0600

    typedef2

---
+++ src/typedef2.dart
@@ -0,0 +1,20 @@
+typedef MultiOperation(int num1, int num2);
+
+Sum(int n1, int n2) {
+  print("Sum of the two number:${n1+n2}");
+}
+Sub(int n1, int n2 ) {
+  print("Subtraction of the two number:${n1-n2}");
+}
+
+NumericOperation(int n1,int n2,MultiOperation mp)
+{
+  print("Inside Operation.....");
+  mp(n1,n2);
+}
+
+void main() {
+
+  NumericOperation(20, 10, Sum);
+  NumericOperation(20, 10, Sub);
+}
