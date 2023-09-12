df8de55c35a9ab11e1a432ba0c8552919c9bb7b7
shahriarsazid <sazid1190849@gamil.com>
2023-09-12 12:18:51 +0600

    generic basic

---
+++ src/Generics.dart
@@ -0,0 +1,31 @@
+//Dart Generics are the same as the Dart collections, which are used to store the homogenous data.
+// As we discussed in the Dart features, it is an optionally typed language.
+
+// void main() {
+//   List <String> logStr = new List <String>();
+//   logStr.add("CHECK");
+//   logStr.add("ERROR");
+//   logStr.add("INFO");
+//
+//   //iterating across list
+//   for (String i in logStr) {
+//     print(i);
+//   }
+// }
+
+
+
+// void main() {
+//   List <String> logStr = new List <String>();
+//   logStr.add(511);   // Add integer value
+//   logStr.add("ERROR");
+//   logStr.add("INFO");
+//
+//   //iterating across list
+//   for (String i in logTypes) {
+//     print(i);
+//   }
+// }
+
+
+
