2e2ea0d46bd95ef5b259710d96b8c1523948d3b6
shahriarsazid <sazid1190849@gamil.com>
2023-09-14 12:23:27 +0600

    generic set

---
+++ src/Generic Set.dart
@@ -0,0 +1,16 @@
+void main() {
+  Set <int>numberSet = new  Set<int>();
+  numberSet.add(10);
+  numberSet.add(20);
+  numberSet.add(30);
+  numberSet.add(40);
+  numberSet.add(50);
+
+  // numberSet.add("");
+  // compilation error;
+  print("Default implementation  :${numberSet.runtimeType}");
+
+  for(var i in numberSet) {
+    print(i);
+  }
+}
