cd0571e437ddc6c489da69e137979d74f372facf
shahriarsazid <sazid1190849@gamil.com>
2023-09-29 00:54:17 +0600

    sort

---
+++ src/Dart_Sort_Part_2.dart
@@ -0,0 +1,22 @@
+void main(){
+  List<String> geek = ["one","two","three","four","five"];
+
+  geek.sort();
+
+  List<int> geeks = [13, 2, -11, 142, -389, 0];
+
+  print(geek);
+
+  print(geeks..sort());
+
+
+  // Creating list of string
+  List<String> demo = ['one', 'two', 'three', 'four'];
+
+  // Sorting string by comparing the length
+  demo.sort((a, b) => a.length.compareTo(b.length));
+
+  print(demo);
+
+
+}
