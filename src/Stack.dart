51a6f04d8caa72b565b50aae0b83f6edcf98c396
Shahriar Islam Sazid <shahriarsazid783@gmail.com>
2023-10-23 10:55:15 +0600

    stack

---
+++ src/Stack.dart
@@ -0,0 +1,14 @@
+
+void main()
+{
+  final stack = Stack<int>();
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  print(stack);
+
+  final element = stack.pop();
+  print('Popped: $element');
+}
+
