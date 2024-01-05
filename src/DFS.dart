6bbffbf52794e6d67a311ecf1c8c15b92f4e2fc8
Shahriar Islam Sazid <shahriarsazid783@gmail.com>
2024-01-05 10:23:50 +0600

    DFS

---
+++ src/DFS.dart
@@ -0,0 +1,44 @@
+class Graph {
+  late int vertices;
+  late List<List<int>> adjacencyList;
+
+   Graph(int vertices) {
+    this.vertices = vertices;
+    adjacencyList = List<List<int>>.generate(vertices, (index) => []);
+  }
+
+  void addEdge(int v, int w) {
+    adjacencyList[v].add(w);
+  }
+
+  void dfs(int startVertex) {
+    Set<int> visited = Set<int>();
+    _dfsHelper(startVertex, visited);
+  }
+
+  void _dfsHelper(int vertex, Set<int> visited) {
+    visited.add(vertex);
+    print('$vertex ');
+
+    for (int neighbor in adjacencyList[vertex]) {
+      if (!visited.contains(neighbor)) {
+        _dfsHelper(neighbor, visited);
+      }
+    }
+  }
+}
+
+void main() {
+  // Example Usage:
+  Graph graph = Graph(6);
+
+  // Adding edges to the graph
+  graph.addEdge(0, 1);
+  graph.addEdge(0, 2);
+  graph.addEdge(1, 3);
+  graph.addEdge(2, 4);
+  graph.addEdge(2, 5);
+
+  print('DFS starting from vertex 0:');
+  graph.dfs(0);
+}
